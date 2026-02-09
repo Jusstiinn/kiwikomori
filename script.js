8be2f05c204a851af2696eda7d7f33c99f2bff5b
Jusstiinn <S10273684@connect.np.edu.sg>
2026-02-09 13:40:49 +0800

    Added other menu options

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function osumichange() {
     document.getElementById("osumi").src = "images/characters/osumi_2.png";
 }
+
+/* get the back button element */
+const backBtn = document.getElementById('back');
+
+/* uncheck all checkboxes when Back button is clicked */
+  backBtn.addEventListener('click', function uncheck() {
+    document.querySelectorAll('input[type="checkbox"]').forEach(cb => {
+      cb.checked = false;
+    });
+  });
