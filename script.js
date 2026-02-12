72517e3128377a611ec98f6e9ff59434032248b9
boothill-lover <s10275365@connect.np.edu.sg>
2026-02-13 01:52:23 +0800

    navigation system

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ const backBtn = document.getElementById('back');
       cb.checked = false;
     });
   });
+
+/* navigation between rooms */
+const rooms = ['north', 'east', 'southFront', 'west'];
+let currentRoom = '0';
+
+function go(where) {
+  const fade = document.getElementById('fade');
+
+  // fade out
+  fade.classList.add('out');
+
+  setTimeout(() => {
+
+    // hide current room and show new room while fading
+    document.querySelector('.room.active').classList.remove('active');
+
+    if (where === 'left') {
+      currentRoom = (currentRoom - 1 + rooms.length) % rooms.length;
+    }
+    // else if going right
+    else {
+      currentRoom = (currentRoom + 1) % rooms.length;
+    }
+
+    // give current room active class
+    document.getElementById(rooms[currentRoom]).classList.add('active');
+
+    // fade in
+    fade.classList.remove('out');
+  }, 300); // duration of fade out
+}
